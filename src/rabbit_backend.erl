9acece044b802a58ab13aa16fa3f9c373856ace7
Antonio Garrote <antonio.garrote@xing.com>
2009-09-01 17:34:31 +0200

    Some fixes for rabbitmq connectivity

--- src/rabbit_backend.erl
+++ src/rabbit_backend.erl
@@ -47,11 +47,8 @@ init(_Arguments) ->
 
 
 handle_call({create, Options}, _From, State) ->
-    log:t(1),
     AlreadyDeclared = proplists:is_defined(proplists:get_value(name,Options),State#rabbit_queue_state.queues),
-    log:t(2),
     if AlreadyDeclared =:= false ->
-            log:t(3),
             try declare_queue(Options, State) of
                 Queue     -> { reply, ok, State#rabbit_queue_state{ queues = [ Queue | State#rabbit_queue_state.queues ] } }
             catch
@@ -75,6 +72,7 @@ handle_call({publish, Content, Queue, BindingKey}, _From, State) ->
 
 handle_call({consume, Function, Queue}, _From, State) ->
     AlreadyDeclared = proplists:is_defined(Queue,State#rabbit_queue_state.queues),
+
     if AlreadyDeclared =:= false ->
             try declare_queue([{queue, Queue}, {bindkey, Queue}], State) of
                 Queue     -> register_consumer(Function, Queue, State),
@@ -105,7 +103,7 @@ channel_default_values() ->
 %% Default values for exchange configuration
 %% options of RabbitMQ
 exchange_default_values() ->
-    [ {type, <<"topic">>},
+    [ {type, <<"direct">>},
       {passive, false},
       {durable, false},
       {auto_delete, false},
@@ -135,13 +133,9 @@ channel_setup(Connection) ->
 %% @doc
 %% Creates a new queue
 declare_queue(Options, State) ->
-    log:t(4),
     Q = proplists:get_value(name, Options),
-    log:t(5),
     X = <<"x">>,
-    log:t(6),
     BindKey = proplists:get_value(bind_key, Options),
-
     QueueDeclare = #'queue.declare'{ticket = State#rabbit_queue_state.ticket,
                                     queue = Q,
                                     passive = false,
@@ -156,7 +150,7 @@ declare_queue(Options, State) ->
 
     ExchangeDeclare = #'exchange.declare'{ticket = State#rabbit_queue_state.ticket,
                                           exchange = X,
-                                          type= <<"topic">>,
+                                          type= <<"direct">>,
                                           passive = false,
                                           durable = false,
                                           auto_delete=false,
@@ -179,17 +173,18 @@ declare_queue(Options, State) ->
 
 publish_content(Content, Queue, BindingKey, State) ->
     QueueState = proplists:get_value(Queue, State#rabbit_queue_state.queues),
+    log:t(["Publishing to:",
+           {queue, Queue},
+           {ticket, State#rabbit_queue_state.ticket},
+           {exchange, QueueState#rabbit_queue.exchange},
+           {routing_key, BindingKey}]),
     BasicPublish = #'basic.publish'{ticket = State#rabbit_queue_state.ticket,
                                     exchange = QueueState#rabbit_queue.exchange,
                                     routing_key = BindingKey,
                                     mandatory = false,
                                     immediate = false},
-    ContentP = #content{class_id = 60,
-                        properties = amqp_util:basic_properties(),
-                        properties_bin = none,
-                        payload_fragments_rev = [Content]
-                       },
-    amqp_channel:cast(State#rabbit_queue_state.channel, BasicPublish, ContentP).
+    Payload = #amqp_msg{payload = log:t(list_to_binary([Content]))},
+    amqp_channel:call(State#rabbit_queue_state.channel, BasicPublish, Payload) .
 
 
 register_consumer(Function, Queue, State) ->
@@ -202,7 +197,10 @@ register_consumer(Function, Queue, State) ->
                                     nowait = false},
 
     ConsumerPid = spawn_consumer(Function, State#rabbit_queue_state.channel),
-    #'basic.consume_ok'{} = amqp_channel:call(State#rabbit_queue_state.channel, BasicConsume, ConsumerPid),
+
+    %#'basic.consume_ok'{ consumer_tag = ConsumerTag } = amqp_channel:call(State#rabbit_queue_state.channel, {BasicConsume, self()}),
+    amqp_channel:subscribe(State#rabbit_queue_state.channel, BasicConsume, ConsumerPid),
+
     ConsumerPid .
 
 
@@ -210,15 +208,16 @@ spawn_consumer(_Function, Channel) ->
     spawn(fun() ->
                   %% If the registration was sucessful, the consumer will
                   %% be notified
-
+                  log:t(1),
                   receive
-                      #'basic.consume_ok'{consumer_tag = ConsumerTag} ->  ok
+                      #'basic.consume_ok'{consumer_tag = ConsumerTag} -> log:t(1),
+                                                                         ok
                   end,
-
+                  log:t(3),
                   ConsumeLoop = fun(F) ->
                                         %% When a message is routed to the queue, it will be
                                         %% delivered to this consumer
-
+                                        log:t(["lets wait for messages"]),
                                         receive
                                             {#'basic.deliver'{delivery_tag = _DeliveryTag}, Content} ->
 
@@ -239,3 +238,110 @@ spawn_consumer(_Function, Channel) ->
                                 end,
                   ConsumeLoop(ConsumeLoop)
           end) .
+
+
+%% tests
+
+
+direct_queue_test() ->
+    %% first get the connection
+    Params = #amqp_params{ username = <<"guest">>,
+                           password = <<"guest">> },
+    ConnectionPid = amqp_connection:start_direct(Params),
+
+    %% Get a channel
+    Access = #'access.request'{ realm = <<"gearman">>,
+                                exclusive = false,
+                                passive = false,
+                                active = true,
+                                write = true,
+                                read = true },
+    Channel = amqp_connection:open_channel(ConnectionPid),
+    #'access.request_ok'{ticket = Ticket} = amqp_channel:call(Channel, Access),
+
+    %% Declare a queue, exchange and binding
+    Q = <<"test">>,
+    X = <<"x">>,
+    BindKey = <<"test_key">>,
+    QueueDeclare = #'queue.declare'{ticket = Ticket,
+                                    queue = Q,
+                                    passive = false,
+                                    durable = false,
+                                    exclusive = false,
+                                    auto_delete = false,
+                                    nowait = false,
+                                    arguments = []},
+    #'queue.declare_ok'{queue = Q,
+                        message_count = _MessageCount,
+                        consumer_count = _ConsumerCount}  = amqp_channel:call(Channel, QueueDeclare),
+
+    ExchangeDeclare = #'exchange.declare'{ticket = Ticket,
+                                          exchange = X,
+                                          type= <<"direct">>,
+                                          passive = false,
+                                          durable = false,
+                                          auto_delete=false,
+                                          internal = false,
+                                          nowait = false,
+                                          arguments = []},
+
+    #'exchange.declare_ok'{} = amqp_channel:call(Channel, ExchangeDeclare),
+
+    QueueBind = #'queue.bind'{ticket = Ticket,
+                              queue = Q,
+                              exchange = X,
+                              routing_key = BindKey,
+                              nowait = false,
+                              arguments = []},
+
+    #'queue.bind_ok'{} = amqp_channel:call(Channel, QueueBind),
+
+    %% Register a consumer
+    BasicConsume = #'basic.consume'{ticket = Ticket,
+                                    queue = Q,
+                                    consumer_tag = <<"">>,
+                                    no_local = false,
+                                    no_ack = true,
+                                    exclusive = false,
+                                    nowait = false},
+
+    ConsumerPid = spawn(fun() ->
+                                %% If the registration was sucessful, the consumer will
+                                %% be notified
+                                receive
+                                    #'basic.consume_ok'{consumer_tag = ConsumerTag} -> ok
+                                end,
+
+                                ConsumeLoop = fun(F) ->
+                                        %% When a message is routed to the queue, it will be
+                                        %% delivered to this consumer
+                                        log:t(["lets wait for messages"]),
+                                        receive
+                                            {#'basic.deliver'{delivery_tag = _DeliveryTag}, Content} ->
+
+                                                log:t("Got something"),
+                                                log:t(["REC:", Content]),
+                                                #amqp_msg{payload = Payload} = Content,
+                                                %% TODO pass the value read to the Function passed as a parameter
+                                                io:format("Message received: ~p~n", [Payload]),
+                                                F(F)
+                                        end
+                                end,
+                                ConsumeLoop(ConsumeLoop)
+                        end),
+
+    amqp_channel:subscribe(Channel, BasicConsume, ConsumerPid),
+
+    %% Let's publish something
+    log:t(["Publishing to:",
+           {queue, Q},
+           {ticket, Ticket},
+           {exchange, X},
+           {routing_key, BindKey}]),
+    BasicPublish = #'basic.publish'{ticket = Ticket,
+                                    exchange = X,
+                                    routing_key = BindKey,
+                                    mandatory = false,
+                                    immediate = false},
+    Payload = #amqp_msg{payload = log:t(list_to_binary(["hola"]))},
+    amqp_channel:call(Channel, BasicPublish, Payload) .
